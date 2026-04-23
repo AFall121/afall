e13852637282da15106cfed36dff068491e72a18
AFallleaves <AFallleaves@noreply.gitcode.com>
2026-04-23 11:39:33 +0800

    modify base option

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -17,12 +17,11 @@ import { h } from "vue";
  * "afall121.github.io/notes/mp3/夜曲.mp3",index.ts这个文件两次"../"后就是在项目根路径下,即
  * 远程的域名下,所以我需要在前面加上"../../"
  */
-const BASE = process.env.VERCEL || process.env.NETLIFY ? "../../" : "../../afall/";
 const playlist = [
   {
     name: "急救室-豪杰春香",
     author: "Izi",
-    url: BASE + "mp3/急救室.mp3",
+    url: "mp3/急救室.mp3",
   },
   {
     name: "夜曲",
@@ -33,31 +32,31 @@ const playlist = [
   {
     name: "飘雪",
     author: "韩雪",
-    url: BASE + "mp3/飘雪.mp3",
+    url: "mp3/飘雪.mp3",
     // url: "https://nf-sycdn.kuwo.cn/8bce4a62c6c67a636de22957c4f1d6ee/6811ff3d/resource/n1/18/36/806287869.mp3?bitrate$128&from=vip",
   },
   {
     name: "雪の華",
     author: "中島美嘉",
-    url: BASE + "mp3/雪の華.mp3",
+    url: "mp3/雪の華.mp3",
     // url: "https://er-sycdn.kuwo.cn/fbe48476a5e9d5ab6aafa19ac4c46705/6811f84c/resource/30106/trackmedia/M500001UboWR2c1rxw.mp3?bitrate$128&from=vip",
   },
   {
     name: "눈의 꽃",
     author: "朴孝信",
-    url: BASE + "mp3/눈의 꽃.mp3",
+    url: "mp3/눈의 꽃.mp3",
     // url: "https://lv-sycdn.kuwo.cn/1f9b177fdd620de1ab97fc9c7b3eb65b/6811fdd9/resource/30106/trackmedia/M500003x8Q1c3xYSPi.mp3?bitrate$128&from=vip",
   },
   {
     name: "Shape Of My Heart",
     author: "外国歌手",
-    url: BASE + "mp3/Shape Of My Heart.mp3",
+    url: "mp3/Shape Of My Heart.mp3",
     // url: "https://a.xmcdn.com/group2/M02/2C/A7/wKgDr1HdXRqAOX2GAEP5jHTqeYA515.mp3?sign=ae3f7301cb989c8e4479898a4286c580&buy_key=www2_32694bde-67498752:0&timestamp=1746003529021000&token=1001&duration=278",
   },
   {
     name: "希望你幸福",
     author: "林享柱",
-    url: BASE + "mp3/希望你幸福.mp3",
+    url: "mp3/希望你幸福.mp3",
     // url: "https://er-sycdn.kuwo.cn/bd36666c78cc155399e63b5ddaed5346/6811efb1/resource/30106/trackmedia/M500003PJyXt2CMVd9.mp3?bitrate$128&from=vip",
   },
 ];
